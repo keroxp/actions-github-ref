81c65c830037375c16f822c8a0a3238dac734740
keroxp <kerokerokerop@gmail.com>
2019-11-18 13:04:09 +0900

    fix

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ try {
   const branchMatch = ref.match(/^refs\/heads\/(.+?)$/);
   const tagMatch = ref.match(/^refs\/tags\/(.+?)$/);
   if (branchMatch) {
-    console.log(`::set-env name=GITHUB_BRANCH::${branchMatch[1]}`);
+    core.exportVariable('GITHUB_BRANCH', branchMatch[1]);
   } else if (tagMatch) {
-    console.log(`::set-env name=GITHUB_TAG::${tagMatch[1]}`);
+    core.exportVariable('GITHUB_TAG', tagMatch[1]);
   }
 } catch (error) {
   core.setFailed(error.message);
